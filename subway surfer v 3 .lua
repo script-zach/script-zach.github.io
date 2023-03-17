04ec30510c16dbc70793b6c5ac2245d2d4c2e8cc
script-zach <128105261+script-zach@users.noreply.github.com>
2023-03-17 08:37:14 -0400

    Add files via upload

---
+++ subway surfer v 3 .lua
@@ -0,0 +1,224 @@
+function Main()
+menu = gg.choice({"🛹 FREE SINGLE USE","♨️ FREE UPGRADES","📦 FREE MYSTERY & TOKEN BOX","📝 FREE SKIP MISSIONS","📀 HACK GOLD","🔑 HACK KEYS","📈 HACK SCORE","Exit Script"},nil,"DECRYPTED BY xAffan SCRIPT BY MORTY")
+if menu==1 then F1()end
+if menu==2 then F2()end
+if menu==3 then F3()end
+if menu==4 then F5()end
+if menu==5 then F6()end
+if menu==6 then F7()end
+if menu==7 then F8()end
+if menu==8 then Exit()end
+vis=false
+end
+
+-----🛹 FREE SINGLE USE-----
+function F1()
+gg.clearResults()
+gg.toast("🚨 Hoverboard...")
+gg.sleep(500)
+gg.toast("🚨 Score Booster...")
+gg.sleep(500)
+gg.toast("🚨 Headstart...")
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.clearResults()
+gg.searchNumber("300;3000;2000", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(100)
+gg.editAll("0", gg.TYPE_DWORD)
+gg.toast("☑️ DONE")
+gg.clearResults()
+end
+
+-----♨️ FREE UPGRADES-----
+function F2()
+gg.clearResults()
+gg.toast("🚨 Jetpack...")
+gg.sleep(500)
+gg.toast("🚨 Super Sneakers...")
+gg.sleep(500)
+gg.toast("🚨 Coin Magnet...")
+gg.sleep(500)
+gg.toast("🚨 2x Multiplier...")
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.clearResults()
+gg.searchNumber("500;1500;3000;10000;30000;60000", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(100)
+gg.editAll("0", gg.TYPE_DWORD)
+gg.toast("☑️ DONE")
+gg.clearResults()
+end
+
+-----📦 FREE MYSTERY & TOKEN BOX-----
+function F3()
+gg.clearResults()
+gg.toast("🚨 Mystery Box...")
+gg.sleep(500)
+gg.toast("🚨 Token Box...")
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.clearResults()
+gg.searchNumber("500;6", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber("500;6", 4, false, 536870912, 0, -1)
+gg.getResults(100)
+gg.editAll("0", gg.TYPE_DWORD)
+gg.toast("☑️ DONE")
+gg.clearResults()
+end
+
+-----📦 FREE MYSTERY & TOKEN BOX-----
+function F4()
+gg.clearResults()
+gg.toast("🚨 Mystery Box...")
+gg.sleep(500)
+gg.toast("🚨 Token Box...")
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.clearResults()
+gg.searchNumber("500;6", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber("500;6", 4, false, 536870912, 0, -1)
+gg.getResults(100)
+gg.editAll("0", gg.TYPE_DWORD)
+gg.toast("☑️ DONE")
+gg.clearResults()
+end
+
+-----📝 FREE SKIP MISSIONS-----
+function F5()
+gg.clearResults()
+gg.toast("🚨 Skip Missions...")
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.clearResults()
+gg.searchNumber("1500", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber("1500", 4, false, 536870912, 0, -1)
+gg.getResults(100)
+gg.editAll("0", gg.TYPE_DWORD)
+gg.toast("☑️ DONE")
+gg.clearResults()
+end
+
+-----📀 HACK GOLD-----
+function F6()
+gg.clearResults()
+gg.toast("🚨 Hack Gold...")
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.clearResults()
+gg.searchNumber("980000", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber("980000", 4, false, 536870912, 0, -1)
+gg.getResults(100)
+gg.editAll("-999999999", gg.TYPE_DWORD)
+gg.alert("📀 Now go in shop and buy [Prince K] for free, after it you get 999999999 Gold")
+gg.toast("☑️ DONE")
+gg.clearResults()
+end
+
+-----🔑 HACK KEYS-----
+function F7()
+gg.clearResults()
+gg.toast("🚨 Hack Keys...")
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.clearResults()
+gg.searchNumber("100;280", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber("280", 4, false, 536870912, 0, -1)
+gg.getResults(100)
+gg.editAll("-999999999", gg.TYPE_DWORD)
+gg.alert("🔑 Now go in shop and buy [Shine Outfit] for [Prince K] for free, after it you get 999999999 Keys")
+gg.toast("☑️ DONE")
+gg.clearResults()
+end
+
+-----📈 HACK SCORE-----
+function F8()
+gg.clearResults()
+searchScore()
+gg.searchNumber(test, gg.TYPE_DWORD)
+gg.getResults(100000009)
+gg.alert("⚠️ After the countdown, enter the new value of your score")
+gg.sleep(999)
+gg.setVisible(false)
+gg.toast("12")
+gg.sleep(999)
+gg.setVisible(false)
+gg.toast("11")
+gg.sleep(999)
+gg.setVisible(false)
+gg.toast("10")
+gg.setVisible(false)
+gg.sleep(999)
+gg.toast("9")
+gg.setVisible(false)
+gg.sleep(999)
+gg.toast("8")
+gg.setVisible(false)
+gg.sleep(999)
+gg.toast("7")
+gg.setVisible(false)
+gg.sleep(999)
+gg.toast("6")
+gg.setVisible(false)
+gg.sleep(999)
+gg.toast("5")
+gg.setVisible(false)
+gg.sleep(999)
+gg.toast("4")
+gg.setVisible(false)
+gg.sleep(999)
+gg.toast("3")
+gg.setVisible(false)
+gg.sleep(999)
+gg.toast("2")
+gg.setVisible(false)
+gg.sleep(999)
+gg.toast("1")
+gg.setVisible(false)
+gg.sleep(999)
+gg.toast("0")
+gg.setVisible(false)
+refineScore()
+gg.refineNumber(p[1], gg.TYPE_DWORD)
+rCount = gg.getResultCount()
+score = gg.getResults(rCount)
+for i, v in ipairs(score) do
+  v.freeze = true
+  v.value = "2147483647"
+  end
+  gg.addListItems(score)
+  gg.clearResults()
+end
+
+function searchScore()
+gg.setVisible(false)
+j = gg.prompt({'Enter your score. Must input or glitch.'}, { [1] = "0" }, { [1] = 'number' })
+  if j == nil then
+  gg.setVisible(false)
+  searchScore()
+  gg.setVisible(false)
+  else
+test = j[1]
+end
+end
+
+function refineScore()
+gg.setVisible(false)
+p = gg.prompt({'Enter new score. Must input or glitch.'}, { [1] = "0" }, { [1] = 'number' })
+  if p == nil then
+  gg.setVisible(false)
+  refineScore()
+  gg.setVisible(false)
+  else
+  end
+end
+
+-----Exit Script-----
+function Exit()
+print("Decrypted by xAffan SCRIPT by MORTY")
+os.exit()
+end
+
+Main() 
+while(true)do
+ if gg.isVisible(true) then
+ vis=true 
+ gg.setVisible(false) 
+ end 
+ gg.clearResults()
+ if vis then
+ Main() 
+end 
+end
